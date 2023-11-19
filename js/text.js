959e2455d76dbb7a2a1aa3991537f3cff1559897
ammar25ahmad <151443372+ammar25ahmad@users.noreply.github.com>
2023-11-19 19:33:26 +0500

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,25 @@
+const texts = ['Welcome to ILM Academy', 'In this Website,', 'You will get', 'Notes', 'Books', 'Model Papers', 'and Pairing Scheme']
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+if(count === texts.length){
+count = 0;
+}
+currentText = texts[count];
+letter = currentText.slice(0, ++index);
+
+document.querySelector('.typing').textContent = letter;
+if(letter.length === currentText.length){
+    count++;
+    index = 0;
+}
+setTimeout(type, 200);
+}());
+
+
+
+  
